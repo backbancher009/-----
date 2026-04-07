b38370073774d5cfb6ee35a972f5e74e40e236f9
backbancher009 <jsjahid20249@gmail.com>
2026-04-07 17:11:41 +0600

    Update promote.js

--- grpUpCmd/promote.js
+++ grpUpCmd/promote.js
@@ -5,7 +5,7 @@ module.exports = {
     console.log(event);
     for (const member of promotedMembers) {
       await api.sendMessage(event.id, {
-        text: `🎉 Congratulations @${member.split('@')[0]}! You are now an admin!`,
+        text: `🎉 𝐂𝐨𝐧𝐠𝐫𝐚𝐭𝐮𝐥𝐚𝐭𝐢𝐨𝐧𝐬 @${member.split('@')[0]}! 𝐘𝐨𝐮 𝐚𝐫𝐞 𝐧𝐨𝐰 𝐚𝐧 𝐚𝐝𝐦𝐢𝐧!`,
         mentions: [member]
       });
     }
