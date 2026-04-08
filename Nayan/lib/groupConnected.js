83f4548c0b340fcb790389d240f79c257ab9bfb4
backbancher009 <jsjahid20249@gmail.com>
2026-04-08 22:23:32 +0600

    Update groupConnected.js

--- Nayan/lib/groupConnected.js
+++ Nayan/lib/groupConnected.js
@@ -1,9 +1,9 @@
 module.exports = function () {
-  return `${global.config.botName} CONNECTED«
+  return `${global.config.botName} 𝐂𝐎𝐍𝐍𝐄𝐂𝐓𝐄𝐃 ✅ 
 
 𝐀𝐬𝐬𝐚𝐥𝐚𝐦𝐮𝐚𝐥𝐚𝐲𝐤𝐮𝐦☘️
 <------------------------------>
 
-𝐙𝐀𝐇𝐈𝐃-𝐁𝐎𝐓 𝐂𝐨𝐧𝐧𝐞𝐜𝐭𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥 !!! 
+𝐙𝐀𝐇𝐈𝐃-𝐁𝐎𝐓 𝐂𝐨𝐧𝐧𝐞𝐜𝐭𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥 !!! 🤖 
 
-DEVELOPER: ${global.config.botOwner}
+𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑: ${global.config.botOwner}
